a9b3ac62ebd7987cecf964f51e85ba7a551925fe
Clara Ballester maymo <clballes@car7s4.42barcelona.com>
2023-03-02 13:27:09 +0100

    fist

--- So_long/srcs/map_errors.c
+++ So_long/srcs/map_errors.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   errors.c                                           :+:      :+:    :+:   */
+/*   map_errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: clballes <clballes@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -9,3 +9,31 @@
 /*   Updated: 2023/02/23 16:02:54 by clballes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+void    ft_checkber(char **argv)
+{
+    int i;
+    i = 0;
+    while (*argv[1] && argv[1][i])
+    {
+        if (argv[1][i] == '.')
+        {
+            i++;
+            if (argv[1][i] == 'b' && argv[1][i + 1] == 'e' && argv[1][i + 2] == 'r')
+            {
+                printf("holaaaa\n");
+                ft_openmap(&argv[1]);
+            }
+            else
+                printf("Not ber extension, try an other map!\n");
+        }
+        i++;
+    }
+}
+
+void    ft_openmap(char **argv)
+{
+         
+}
--- So_long/srcs/so_long.c
+++ So_long/srcs/so_long.c
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: clballes <clballes@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/02 12:52:23 by clballes          #+#    #+#             */
-/*   Updated: 2023/03/02 12:52:25 by clballes         ###   ########.fr       */
+/*   Created: 2023/03/02 12:52:14 by clballes          #+#    #+#             */
+/*   Updated: 2023/03/02 12:52:15 by clballes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SOLONG_H
-# define SOLONG_H
-# include <stddef.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-void    ft_checkber(char **argv);
-void    ft_openmap(char **argv);
+#include "../inc/so_long.h"
 
-#endif
+int main(int argc, char **argv)
+{
+    if (argc > 1 && *argv[1])
+        ft_checkber(argv);
+    else
+        printf("missing map!\n");
+}
